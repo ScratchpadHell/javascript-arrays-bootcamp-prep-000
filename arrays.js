8c030920a96041cc429c9bf3cebe928ec24f1cb2
Jacques Bellisario <gypsyoak@yahoo.com>
2018-03-22 04:18:08 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,2 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
-function addElementToBeginningOfArray
+function addElementToBeginningOfArray(x, y) {
+  
+}
+function destructivelyAddElementToBeginningOfArray(x, y) {
+  
+}
