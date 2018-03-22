c6e7cd128926f1717ea88f4d0b08deca05690e1e
Jacques Bellisario <gypsyoak@yahoo.com>
2018-03-22 05:02:23 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,7 +2,7 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray() {
   chocolateBars.unshift(0);
-  return chocolateBars
+  return chocolateBars;
 }
   
 function destructivelyAddElementToBeginningOfArray() {
